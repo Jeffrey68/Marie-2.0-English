cb6f81c0da54b0904bc4e073141159d366619513
Jeffrey68 <nathanjmartin05@gmail.com>
2020-03-30 13:22:31 +0100

    Update misc.py

--- tg_bot/modules/misc.py
+++ tg_bot/modules/misc.py
@@ -265,7 +265,7 @@ def kill(bot: Bot, update: Update, args: List[str]):
     melee = random.choice(MELEE)
     guns = random.choice(GUNS)
 
-    repl = temp.format(user1=user1, user2=user2, guns=Guns, melee=Melee)
+    repl = temp.format(user1=user1, user2=user2, guns=guns, melee=melee)
 
     reply_text(repl, parse_mode=ParseMode.MARKDOWN)
     
