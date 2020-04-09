50cbfad4db15b3d6a5a95670e68c26ae205e7cef
Jeffrey68 <nathanjmartin05@gmail.com>
2020-04-09 20:35:37 +0100

    Update notes.py

--- tg_bot/modules/notes.py
+++ tg_bot/modules/notes.py
@@ -12,7 +12,7 @@ from telegram.utils.helpers import escape_markdown
 import tg_bot.modules.sql.notes_sql as sql
 from tg_bot import dispatcher, MESSAGE_DUMP, LOGGER
 from tg_bot.modules.disable import DisableAbleCommandHandler
-from tg_bot.modules.helper_funcs.chat_status import user_admin
+from tg_bot.modules.helper_funcs.chat_status import user_admin, is_user_admin
 from tg_bot.modules.helper_funcs.misc import build_keyboard, revert_buttons
 from tg_bot.modules.helper_funcs.msg_types import get_note_type
 
